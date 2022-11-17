14c5f4b065a77f67601e4c57b865eb6d28bf2bce
podvalniypiero <podvalniypiero@mail.ru>
2022-11-17 11:51:04 +0300

    made Header as a component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
+import React from 'react';
 import './App.scss';
+import Header from './components/Header';
+
 
 
 function App() {
   return (
     <div className="wrapper">
-     <header className='header-container'>
-      <ul>
-      <li className='header top'>Главная</li>
-      <li className='header top'>Новости</li>
-      <li className='header top'>Размещение и тарифы</li>
-      <li className='header top'><img width="8" height="10"src="/img/map.svg" alt="На карте" className='map'/>Объявления на карте</li>
-      <li className='header top'>Контакты</li>
-      <li className='header top'>Закладки<img width="16" height="16" src="/img/favourite.png" alt="Закладки" className='favourite'/></li>
-      <li className='header top entry'>Вход и регистрация</li>
-      </ul>
-
-     </header>
+     <Header/>
       
      </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+
+import styles from './Header.module.scss';
+
+
+function Header(){
+  return(
+<header>
+      <div className={styles['header-container-top']}>
+
+      <ul className={styles['header-top']}>
+        <li>Главная</li>
+        <li>Новости</li>
+        <li>Размещение и тарифы</li>
+        <li><img width="8" height="10"src="/img/map.svg" alt="На карте" className={styles.map}/>Объявления на карте</li>
+        <li>Контакты</li>
+        <li>Закладки<img width="16" height="16" src="/img/favourite.png" alt="Закладки" className={styles.favourite}/></li>
+        <li className={styles.entry}>Вход и регистрация</li>
+      </ul>
+      </div>
+
+      <div className={styles['header-container-bottom']}>
+        <ul className={styles['header-bottom']}>
+          <li><img width="" height=""src="/img/logo.png" alt="SDAEM.BY" className={styles.logo}/></li>
+          <li><img width="8" height="10"src="/img/map.svg" alt="На карте" className={styles.map}/>Квартиры на сутки</li>
+          <li>Коттеджи и усадьбы</li>
+          <li>Бани и Сауны</li>
+          <li>Авто напрокат</li>
+          <li><button className={styles['header-button']}>+ Разместить объявление</button></li>
+        </ul>
+      </div>
+
+     </header>
+  )
+}
+
+export default Header;
